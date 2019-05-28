6927f52d7924cf0cc04e675c5fd8b07b55a19373
szigony <szigeti.patrik@gmail.com>
2019-05-28 07:30:07 +0200

    Clean LifeExpectancy.csv and CountriesOfTheWorld.xls.

--- data-preparation.R
+++ data-preparation.R
@@ -1,6 +1,8 @@
 # Libraries
 library(dplyr)
 library(tibble)
+library(stringr)
+library(readxl)
 
 # Drinks.csv
 drinks <- as.tibble(read.csv("data/Drinks.csv")) %>% 
@@ -17,6 +19,39 @@ drinks <- as.tibble(read.csv("data/Drinks.csv")) %>%
   select(country, beer_servings, wine_servings, spirit_servings, total_litres_of_pure_alcohol)
 
 # LifeExpectancy.csv
+life_expectancy <- as.tibble(read.csv("data/LifeExpectancy.csv")) %>% 
+  mutate(country = CountryDisplay,
+         year = YearCode,
+         metric = GhoDisplay,
+         region = RegionDisplay,
+         income_group = WorldBankIncomeGroupDisplay,
+         sex = SexDisplay,
+         value = Numeric) %>% 
+  select(country, year, metric, region, income_group, sex, value) %>% 
+  mutate(income_group = na_if(str_replace(str_replace(income_group, "_income", ""), "_", " "), "")) %>% 
+  mutate_each(list(as.character), country)
+unique(life_expectancy$metric)
 
+# CountriesOfTheWorld.xls
+countries <- read_excel("data/CountriesOfTheWorld.xls", sheet = "Sheet1", range = "A4:P232")
+names(countries) <- paste(names(countries), countries[1, ], sep = " ")
 
-# CountriesOfTheWorld.xlsx
+countries <- countries %>% 
+  slice(2:n()) %>% 
+  mutate(country = `Country NA`,
+         population = `Population NA`,
+         area = as.numeric(`Area sq. mi.`),
+         population_density = as.numeric(`Pop. Density per sq. mi.`),
+         coast_area_ratio = as.numeric(`Coastline coast/area ratio`),
+         net_migration = `Net migration NA`,
+         infant_mortality_per_1000_births = as.numeric(`Infant mortality per 1000 births`),
+         gdp_per_capita = as.numeric(`GDP $ per capita`),
+         literacy = as.numeric(`Literacy %`) / 100,
+         phones_per_1000 = as.numeric(`Phones per 1000`),
+         arable = as.numeric(`Arable %`) / 100,
+         crops = as.numeric(`Crops %`) / 100,
+         other = as.numeric(`Other %`) / 100,
+         birthrate = `Birthrate NA`,
+         deathrate = `Deathrate NA`) %>% 
+  select(country, population, area, population_density, coast_area_ratio, net_migration, infant_mortality_per_1000_births,
+         gdp_per_capita, literacy, phones_per_1000, arable, crops, other, birthrate, deathrate)
